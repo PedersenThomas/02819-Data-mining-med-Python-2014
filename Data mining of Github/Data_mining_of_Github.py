3dcbddd444bdbc77abc3619005ad47242a95d3fc
Kim Knutzen <kimknutzen@gmail.com>
2014-09-22 18:02:10 +0200

    Request github api and print json

--- Data mining of Github/Data_mining_of_Github.py
+++ Data mining of Github/Data_mining_of_Github.py
@@ -1,2 +1,12 @@
 #Authers. Kim Knutzen & Thomas Pedersen
 print('Hello Python World')
+
+import requests
+import json
+import pprint
+
+content = json.loads(requests.get("https://api.github.com/events", headers={'User-Agent': "kknutzen/1.0"}).content)
+
+#pp = pprint.PrettyPrinter(indent = 1)
+#pp.pprint(content)
+print json.dumps(content, indent=2)

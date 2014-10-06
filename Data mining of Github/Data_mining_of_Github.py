ba42f6ff8228bd28bb932ba161a866ef58c935aa
Thomas Pedersen <cooltomme@gmail.com>
2014-10-06 14:33:27 +0200

    Updated a few lines, to comply with flake8.

--- Data mining of Github/Data_mining_of_Github.py
+++ Data mining of Github/Data_mining_of_Github.py
@@ -1,4 +1,4 @@
-#Authers. Kim Knutzen & Thomas Pedersen
+# Authers. Kim Knutzen & Thomas Pedersen
 print('Hello Python World')
 
 import requests
@@ -8,9 +8,11 @@ import datetime
 
 db = MongoClient('mongodb://python:python@www.k-development.dk/local').local
 
-while datetime.datetime.utcnow() < datetime.datetime(2014,10,5):
+while datetime.datetime.utcnow() < datetime.datetime(2014, 10, 5):
     print "-------------------------------------------------------"
-    content = json.loads(requests.get("https://api.github.com/events", headers={'User-Agent': "kknutzen/1.0"}, auth=requests.auth.HTTPBasicAuth('user', 'pass')).content)
+    content = json.loads(requests.get("https://api.github.com/events",
+                                      headers={'User-Agent': "kknutzen/1.0"},
+                                      auth=requests.auth.HTTPBasicAuth('user', 'pass')).content)
     for data in content:
         obj = next(db.github.find({'id': data['id']}), None)
         if not obj:
@@ -18,4 +20,3 @@ while datetime.datetime.utcnow() < datetime.datetime(2014,10,5):
             print '.'
         else:
             print 'Duuuub'
-

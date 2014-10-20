bf4b955638669501c19747a65fbfb6fb46f3207c
Kim Knutzen <kimknutzen@gmail.com>
2014-10-20 16:17:29 +0200

    Moved configuration reader to configuration.py

--- Data mining of Github/Data_mining_of_Github.py
+++ Data mining of Github/Data_mining_of_Github.py
@@ -1,34 +1,18 @@
 # Authers. Kim Knutzen & Thomas Pedersen
-print('Hello Python World')
 
-import ConfigParser
 import requests
 import json
 from pymongo import MongoClient
+import configuration
 import datetime
 
-def read_config():
-    config = ConfigParser.ConfigParser({'port':'27017'})
-    config.read('config.cfg')
 
-    sec_Database = 'Database'
-    db_user = config.get(sec_Database, 'user')
-    db_pw = config.get(sec_Database, 'password')
-    db_host = config.get(sec_Database, 'host')
-    db_port = config.get(sec_Database, 'port')
-    database = config.get(sec_Database, 'database')
-
-    sec_Github = 'Github'
-    git_user = config.get(sec_Github, 'user')
-    git_pw = config.get(sec_Github, 'password')
-    git_useragent = config.get(sec_Github, 'user-agent')
-
-
-
-def mine_github_events(while_predicate):
+def mine_github_events(cfg):
+    credentials = requests.auth.HTTPBasicAuth(cfg.git_user, cfg.git_password)
     response = requests.get("https://api.github.com/events",
-                            headers={'User-Agent': git_useragent},
-                            auth=requests.auth.HTTPBasicAuth(git_user, git_pw))
+                            headers={'User-Agent': cfg.user_agent},
+                            auth=credentials)
+
     events = json.loads(response.content)
     for event in events:
         obj = next(db.github.find({'id': event['id']}), None)
@@ -40,13 +24,13 @@ def row_exists(id):
     return doc != None
 
 if __name__ == '__main__':
-    read_config()
-    dsn = 'mongodb://{0}:{1}@{2}:{3}/{4}'.format(db_user, 
-                                                 db_pw, 
-                                                 db_host, 
-                                                 db_port, 
-                                                 database)
+    cfg = configuration.Configuration('config.cfg')
+    dsn = 'mongodb://{0}:{1}@{2}:{3}/{4}'.format(cfg.db_user, 
+                                                 cfg.db_password, 
+                                                 cfg.db_host, 
+                                                 cfg.db_port, 
+                                                 cfg.db_database)
     db = MongoClient(dsn).local
 
     while datetime.datetime.utcnow() < datetime.datetime(2014, 10, 5):
-        mine_github_events()
+        mine_github_events(cfg)
--- Data mining of Github/configuration.py
+++ Data mining of Github/configuration.py
@@ -8,4 +8,33 @@ class Configuration(object):
         self.config.read(filename)
 
     @property
-    def 
+    def db_user(self):
+        return self.config.get('Database', 'user')
+
+    @property
+    def db_password(self):
+        return self.config.get('Database', 'password')
+
+    @property
+    def db_host(self):
+        return self.config.get('Database', 'host')
+
+    @property
+    def db_port(self):
+        return self.config.get('Database', 'port')
+
+    @property
+    def db_database(self):
+        return self.config.get('Database', 'database')
+
+    @property
+    def git_user(self):
+        return self.config.get('Github', 'user')
+
+    @property
+    def git_password(self):
+        return self.config.get('Github', 'password')
+
+    @property
+    def user_agent(self):
+        return self.config.get('Github', 'user-agent')

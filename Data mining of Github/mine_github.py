84548753eb8dbf5d986de7aa4f76f53627fb2c89
Kim Knutzen <kimknutzen@gmail.com>
2014-10-27 15:46:32 +0100

    preprocessing of events based on repo

--- Data mining of Github/mine_github.py
+++ Data mining of Github/mine_github.py
@@ -1,8 +1,16 @@
 """Datamining Guthub.
 
-Usage: mine_github <until>
+Usage: 
+    mine_github mine --until <date>
+    mine_github preprocess
+    mine_github -h | --help
 
-Options: --until  Mines until specified date as 'yyyy-mm-dd'
+Arguments:
+    <date>          The desired date to mine until 'yyyy-mm-dd'
+
+Options:
+    -h --help       Show help message 
+    --until <date>  Specify stopping date for mining
   
 """
 
@@ -26,6 +34,35 @@ def mine_github_events(cfg):
         if not obj:
             db.github.insert(event)
 
+def preprocess(dsn):
+    db = MongoClient(dsn).local
+    
+    #
+    # How to iterate over large datasets
+    # https://groups.google.com/forum/#!topic/mongodb-user/YQGOPZjRtlY
+    # http://docs.mongodb.org/manual/tutorial/create-tailable-cursor/
+    #
+    for event in db.github.find( timeout=False ):
+        name = event["repo"]["name"]
+        type = event["type"]
+        object_id = event["_id"]
+
+        #Lookup existing repo
+        repo = next(db.repos.find({'name': name}), None)
+        if not repo:
+            #insert repo 
+            repo = {"name" : name, "event" : {type : [object_id]}}
+            db.repos.insert(repo)
+        else:
+            #Update the existing by appending the event
+            if repo["event"].has_key(type):
+                if not object_id in repo["event"][type]:
+                    repo["event"][type].append(object_id)
+            else:
+                repo["event"][type] = [object_id]
+            db.repos.update({'_id':repo["_id"]}, 
+                            {"$set": {"event" : repo["event"]}}, upsert=False)
+            
 
 def row_exists(id):
     doc = next(db.github.find({'id': id}), None)
@@ -34,14 +71,22 @@ def row_exists(id):
 
 if __name__ == '__main__':
     args = docopt(__doc__)
-    until = datetime.strptime(args["<until>"], "%Y-%m-%d")
     cfg = configuration.Configuration('config.cfg')
     dsn = 'mongodb://{0}:{1}@{2}:{3}/{4}'.format(cfg.db_user, 
                                                  cfg.db_password, 
                                                  cfg.db_host, 
                                                  cfg.db_port, 
                                                  cfg.db_database)
-    db = MongoClient(dsn).local
 
-    while datetime.utcnow() < until:
-        mine_github_events(cfg)
+    if args["mine"]:
+        until = datetime.strptime(args["<until>"], "%Y-%m-%d")
+        db = MongoClient(dsn).local
+
+        while datetime.utcnow() < until:
+            mine_github_events(cfg)
+    
+    elif args["preprocess"]:
+        preprocess(dsn)
+
+    else:
+        print "Command not recognized"
